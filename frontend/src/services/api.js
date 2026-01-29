51b21ae0aa79c9a2fd8cf095c84624da397f12d7
Can Sever <cansever1005@gmail.com>
2026-01-29 15:29:16 +0300

    refined frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
 
 import axios from 'axios';
 
-// Backend URL'i (Cihangir deploy edene kadar bunu kullan)
+// Backend URL'i (Deploy edilene kadar bu kullanılacak.)
 const API_URL = "http://127.0.0.1:8000/api/v1";
 
 export const optimizePromptService = async (userInput) => {
   try {
-    // Backend'e gönderilecek paket (Kaptanın belirlediği kurala uygun)
+    // Backend'e gönderilecek paket
     const payload = {
       userID: "test-user-ece", // Şimdilik sabit
       inputPrompt: userInput,
